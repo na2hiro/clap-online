e4d5783d896b90187788bf1acbba8bfcbf2fa92a
na2hiro <a@81.la>
2013-12-28 03:02:04 +0900

    人数

--- clap.js
+++ clap.js
@@ -10,10 +10,13 @@ app.get('/', function (req, res) {
 app.get('/clap.mp3', function (req, res) {
   res.sendfile(__dirname + '/clap.mp3');
 });
-
+var member = 0;
 io.sockets.on('connection', function (socket) {
-  socket.on('clap', function (data) {
-	socket.broadcast.emit('clap');
-	socket.emit('clap');
-  });
+	io.sockets.emit("member", ++member);
+	socket.on('clap', function (data) {
+		io.sockets.emit('clap');
+	});
+	socket.on("disconnect", function(){
+		io.sockets.emit("member", --member);
+	});
 });
